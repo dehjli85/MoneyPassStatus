e88464109cf70cab7c40a0693b7d3045fb334903
Dennis Li <dennis.hj.li@gmail.com>
2014-10-11 12:16:11 -0700

    api and sesion key dynamically pulled

--- app/models/money.rb
+++ app/models/money.rb
@@ -7,10 +7,27 @@ class Money
 
 	def self.getATMs(searchTerm)
 
+
+		#make the http request to moneypass site to get credentials
+		uri = URI('http://atmlocator.moneypass.com/InitializeSettings.ashx')
+		http = Net::HTTP.new(uri.host, uri.port)
+		req = Net::HTTP::Get.new('http://atmlocator.moneypass.com/InitializeSettings.ashx')
+		http.start
+		res = http.request(req)
+
+		#parse response to get the security key
+		sessionKeyMatches = res.body.match(/\"apiKey\":\"(\w+)\"/)
+		sessionKey = sessionKeyMatches[1]
+		apiKeyMatches = res.body.match(/\"key\":\"([\w|-]*)\"/)
+		apiKey = apiKeyMatches[1]
+
+		#close the connection
+		http.finish
+
 		######query for geolocation
 		#set the geolocation URL
 		geoLocUrl = 'http://dev.virtualearth.net/REST/v1/Locations/?'
-		geoLocUrl += 'key=AkmEVpsTKVmExV9nyxo3DEJbd1YXqup_Wvv4s84f1zwA0czk0wJX-1aMnqSmCnSS'
+		geoLocUrl += 'key=' + apiKey
 		geoLocUrl += '&includeNeighborhood=1'
 		geoLocUrl += '&q=' + URI::encode(searchTerm.to_s)
 		geoLocUrl += '&maxResults=25'
@@ -40,7 +57,7 @@ class Money
 		moneypassAPIurl = 'https://locatorapi.moneypass.com/Service.svc/locations/atm?'
 		moneypassAPIurl += '_dc=1409595874934'
 		moneypassAPIurl += '&format=json'
-		moneypassAPIurl += '&key=SFoS4aJaTztUVy3'
+		moneypassAPIurl += '&key=' + sessionKey
 		moneypassAPIurl += '&spatialFilter=nearby(' + latitude.to_s + '%2C' + longitude.to_s + '%2C100)'
 		moneypassAPIurl += '&filter=(SearchDBAName%20%3D%20%27WALMART%27)'
 		moneypassAPIurl += '&start=0'
