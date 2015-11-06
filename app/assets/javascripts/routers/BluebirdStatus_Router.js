d4d72ecd17bea9ae6a127b6fde4aac315b696c20
Dennis Li <dennis@sowntogrow.com>
2015-11-06 00:25:35 -0800

    added favicon

--- app/assets/javascripts/routers/BluebirdStatus_Router.js
+++ app/assets/javascripts/routers/BluebirdStatus_Router.js
@@ -7,21 +7,13 @@ BluebirdStatus.module("App", function(App, BluebirdStatus, Backbone, Marionette,
 	});
 
 	var API = {
-		showIndex: function(){
-			console.log("route to home was triggered, showActionButtons");
-			App.Controller.showActionButtons();			
-			
-		},
+		
 
 		
 		
 	};
 
-	BluebirdStatus.on("index:home",function(){
-		BluebirdStatus.navigate("home");
-		API.showIndex();
-	});
-
+	
 	BluebirdStatus.addInitializer(function(){
 		new App.Router({
 			controller: API
